ebb34b8ee55de5933ea68ce0abcc4167c1d54c1f
3ele-projects <github@3ele.de>
2020-01-31 13:32:47 +0100

    Submenu skizze

--- gomedia-child/menu-secondary.php
+++ gomedia-child/menu-secondary.php
@@ -3,47 +3,46 @@
 $kategorie = get_the_category($post->ID);
 $kategorie = $kategorie[0];
 $name_der_kategorie = $kategorie->name;
-$name_der_kategorie_klein = strtolower($name_der_kategorie);?>
+$category_ID = $kategorie->ID;
+$site_url = get_site_url();
+$homeID = get_option('page_on_front');
+$category_link = get_category_link( $kategorie ); ?>
 
-	<nav id="secondary-nav" class="main-navigation" role="navigation">
+	<nav id="secondary-nav" class="main-navigation navbar navbar-light navbar-expand-lg " role="navigation">
 		<div class="container">
 			<div class="row">
 
-				<ul id="secondary-menu" class="sf-menu col-md-9 l_tinynav1 sf-js-enabled sf-arrows">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			Menü aufklappen
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	<?php if ( is_single()) : ?>	
-					
-	<li id="menu-item-710" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-710" data-children-count="0"><a href="https://360kompakt.de" data-wpel-link="internal" target="_self" title="360Kompakt">Zur Startseite</a></li>
-					<li id="menu-item-709" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-709" data-children-count="0"><a href="https://360kompakt.de/<?php echo $name_der_kategorie_klein; ?>" data-wpel-link="internal" target="_self" title="<?php echo $name_der_kategorie; ?>">Weitere Artikel zum Thema <?php echo $name_der_kategorie; ?></a></li>
-					
+		<ul id="secondary-menu" class="sf-menu col-md-9 l_tinynav1 sf-js-enabled sf-arrows">
+			
+	<li id="menu-item-<?php echo $homeID;?>" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-<?php echo $homeID;?>" data-children-count="0"><a href="<?php echo $site_url;?>" data-wpel-link="internal" target="_self" title="360Kompakt">Zur Startseite</a></li>
+					<li id="menu-item-<?php echo $category_ID ?>" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-709" data-children-count="0"><a href="<?php echo $category_link; ?>" data-wpel-link="internal" target="_self" title="<?php echo $name_der_kategorie; ?>">Weitere Artikel zum Thema <?php echo $name_der_kategorie; ?></a></li>
+		
+</ul>			
 	<?php else : ?>
-	<li id="menu-item-712" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-712" data-children-count="0"><a href="https://360kompakt.de/finanzen" data-wpel-link="internal" target="_self" title="Finanzen">Finanzen</a></li>
-	
-	<li id="menu-item-713" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-713" data-children-count="0"><a href="https://360kompakt.de/technologie" data-wpel-link="internal" target="_self" title="Technologie">Technologie</a></li>
-	
-	
-	
-	<li id="menu-item-714" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-714" data-children-count="0"><a href="https://360kompakt.de/lifestyle" data-wpel-link="internal" target="_self" title="Lifestyle">Lifestyle</a></li>
-	
-	
-	
-	<li id="menu-item-715" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-715" data-children-count="0"><a href="https://360kompakt.de/management" data-wpel-link="internal" target="_self" title="Management">Management</a></li>
-	
-	
-	
-	<li id="menu-item-716" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-716" data-children-count="0"><a href="https://360kompakt.de/marketing" data-wpel-link="internal" target="_self" title="Marketing">Marketing</a></li>
-	
-	
-	
-	<li id="menu-item-717" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-717" data-children-count="0"><a href="https://360kompakt.de/recht" data-wpel-link="internal" target="_self" title="Recht">Recht</a></li>
+
+		<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'secondary',
+						'container'       => '',
+						'menu_id'         => 'menu-second_menu',
+						'menu_class'      => 'navbar-nav mr-auto sf-menu col-md-9 sf-arrows',
+						'fallback_cb'     => ''
+					)
+				); ?>
+
 					
 	<?php endif; ?>
 
 
-</ul>
 				
 				
 				<?php get_search_form(); // Loads the searchform.php template. ?>
-
+				</div>
 			</div>
 		</div><!-- .container -->
 	</nav><!-- #secondary-nav -->
